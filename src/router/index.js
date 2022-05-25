7f71ecfaf12858a19e1097a204feff1eebd285da
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-05-25 16:27:51 -0300

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,20 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    meta: {
+      title: "IP Tracker Address",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="flex flex-col h-screen max-h-screen">
+    <!--    Search / Results-->
+    <div
+      class="z-20 bg-hero-pattern flex justify-center relative px-4 pt-8 pb-32"
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            v-model="queryIp"
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+            @keyup.enter="getIpInfo"
+          />
+          <i
+            @click="getIpInfo"
+            class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fa-solid fa-chevron-right"
+          />
+        </div>
+      </div>
+      <!--      IP Info-->
+      <IPInfo v-if="ipInfo" :ipInfo="ipInfo" />
+    </div>
+    <!--    Map -->
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import IPInfo from "@/components/IPInfo";
+import leaflet from "leaflet";
+import axios from "axios";
+import { onMounted, ref } from "vue";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  components: { IPInfo },
+  setup() {
+    const queryIp = ref("");
+    const ipInfo = ref(null);
+    let mymap;
+    onMounted(() => {
+      mymap = leaflet.map("mapid").setView([42.5145, -83.0147], 9);
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFjdWFndWVyaWEiLCJhIjoiY2wweWt2OWl2MTJrMzNqam5vand3Mm95dCJ9.f0hIj3Ec8IcFrvlJ9iZitA",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1IjoiZmFjdWFndWVyaWEiLCJhIjoiY2wweWt2OWl2MTJrMzNqam5vand3Mm95dCJ9.f0hIj3Ec8IcFrvlJ9iZitA",
+          }
+        )
+        .addTo(mymap);
+    });
+
+    const getIpInfo = async () => {
+      try {
+        const data = await axios.get(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_9UBJVPLMoPnvMmt0g4ULtXIP4O4BH&ipAddress=${queryIp.value}`
+        );
+        const result = data.data;
+        console.log(result);
+        ipInfo.value = {
+          address: result.ip,
+          state: result.location.region,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(mymap);
+        mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+
+    return { queryIp, ipInfo, getIpInfo };
   },
 };
 </script>
